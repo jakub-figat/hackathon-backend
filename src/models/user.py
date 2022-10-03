9e68b8178524a88fe3ff0b826788b531be2da33f
Rafał Fiedosiuk <fiedosiukr@gmail.com>
2022-10-03 22:52:47 +0200

    Add user fields and remove password validation.

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import (
+    Boolean,
     Column,
     Date,
     String,
@@ -11,3 +12,8 @@ class UserModel(Base):
     email = Column(String(length=100), nullable=False, unique=True)
     password = Column(String(length=100), nullable=False)
     date_of_birth = Column(Date, nullable=False)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    phone_number = Column(String(50), nullable=True)
+    image = Column(String(), nullable=True)
+    is_verified = Column(Boolean(), nullable=False, default=False)
--- src/schemas/user/dto.py
+++ src/schemas/user/dto.py
@@ -1,8 +1,6 @@
 import datetime as dt
-from typing import Any
 from uuid import UUID
 
-from pydantic.class_validators import validator
 from pydantic.fields import Field
 from pydantic.networks import EmailStr
 
@@ -13,14 +11,6 @@ class UserRegisterSchema(BaseModel):
     email: EmailStr
     date_of_birth: dt.date
     password: str = Field(..., min_length=8, max_length=35)
-    password_2: str = Field(..., min_length=8, max_length=35)
-
-    @validator("password_2")
-    def validate_passwords(cls, password_2: str, values: dict[str, Any]) -> str:
-        if password_2 != values["password"]:
-            raise ValueError("Passwords do not match")
-
-        return password_2
 
 
 class UserLoginSchema(BaseModel):
