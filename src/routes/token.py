6acb4d2808214c99d36c62cd39c9b95488d4f5aa
jakub-figat <jakub.figat@neontri.com>
2022-10-05 17:55:00 +0200

    Add more tests for token routes

--- src/routes/token.py
+++ src/routes/token.py
@@ -64,4 +64,4 @@ async def refresh_token_pair(
 
         return {"detail": "Successfully logged in"}
     except InvalidToken:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token.")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token")
--- tests/integration/test_routes/test_token.py
+++ tests/integration/test_routes/test_token.py
@@ -6,9 +6,11 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.app import app
+from src.data_access.jwt import RefreshTokenDataAccess
 from src.data_access.user import UserDataAccess
 from src.deps.db import get_async_session
 from src.schemas.user.data_access import UserInputSchema
+from src.services.jwt import TokenService
 from src.utils.password import password_context
 
 
@@ -44,10 +46,43 @@ async def user_data_access_with_user(
     return data_access
 
 
+@pytest.fixture(scope="function")
+async def refresh_token_in_db(
+    register_schema: UserInputSchema, user_data_access_with_user: UserDataAccess, async_test_session: AsyncSession
+) -> str:
+    service = TokenService(
+        user_data_access=user_data_access_with_user,
+        refresh_token_data_access=RefreshTokenDataAccess(session=async_test_session),
+    )
+    token_pair = await service.generate_token_pair(login_schema=register_schema)
+    return token_pair.refresh_token
+
+
 async def test_token_routes_login(
     http_client: AsyncClient, register_schema: UserInputSchema, user_data_access_with_user: UserDataAccess
 ) -> None:
     response = await http_client.post(url="/token/login/", json=register_schema.dict(include={"email", "password"}))
+    assert response.status_code == status.HTTP_200_OK
+
+    assert "refresh_token" in response.cookies
+    assert "access_token" in response.cookies
+
+
+async def test_token_routes_login_with_invalid_credentials(http_client: AsyncClient) -> None:
+    response = await http_client.post(
+        url="/token/login/", json={"email": "invalid@invalid.com", "password": "password12345678"}
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert len(response.cookies) == 0
+
+
+async def test_token_routes_refresh_token_pair(
+    register_schema: UserInputSchema, http_client: AsyncClient, user_data_access_with_user: UserDataAccess
+) -> None:
+    token_response = await http_client.post(
+        url="/token/login/", json=register_schema.dict(include={"email", "password"})
+    )
+    response = await http_client.post(url="/token/refresh/", cookies=token_response.cookies)
     assert response.status_code == status.HTTP_200_OK
 
     assert "refresh_token" in response.cookies
